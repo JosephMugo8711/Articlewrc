55d6e41c0aa11632648220f3b95d48abdd75cc87
Joseph <wamiti8711@gmail.com>
2023-02-20 15:25:04 -0500

    author

--- lib/author.rb
+++ lib/author.rb
@@ -1,23 +1,23 @@
-# class Author
-#     attr_reader :name
+class Author
+    attr_reader :name
     
-#     def initialize(name)
-#       @name = name
-#     end
+    def initialize(name)
+      @name = name
+    end
     
-#     def add_article(magazine, title)
-#       Article.new(self, magazine, title)
-#     end
+    # def add_article(magazine, title)
+    #   Article.new(self, magazine, title)
+    # end
     
-#     def articles
-#       Article.all.select { |article| article.author == self }
-#     end
+    # def articles
+    #   Article.all.select { |article| article.author == self }
+    # end
     
-#     def magazines
-#       articles.map { |article| article.magazine }.uniq
-#     end
+    # def magazines
+    #   articles.map { |article| article.magazine }.uniq
+    # end
     
-#     def topic_areas
-#       magazines.map { |magazine| magazine.category }.uniq
-#     end
-# end
+    # def topic_areas
+    #   magazines.map { |magazine| magazine.category }.uniq
+    # end
+end
