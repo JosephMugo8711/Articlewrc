17f2b8be6a589d74b971f2cc92d99c081a2e6c68
Joseph <wamiti8711@gmail.com>
2023-02-20 15:33:10 -0500

    final commit testing

--- test/test.rb
+++ test/test.rb
@@ -7,17 +7,3 @@ end
 
 binding.pry
 
-# Author1 = Author.new(“Vivian”)
-# Author2 = Author.new(“Fred”)
-# Author3 = Author.new(“Lilian”)
-# Author4 = Author.new(“Leah”)
-# Author5 = Author.new(“Joseph”)
-# Author6 = Author.new(“Kelvin”)
-
-# magazine1 = Magazine.new(“Standard”, “Fashion”)
-# magazine2 = Magazine.new(“Star”, “Politics”)
-# magazine3 = Magazine.new(“Nation”, “Sports”)
-# magazine4 = Magazine.new(“Digital”, “Weather”)
-
-# article1 = Article.new(author1, magazine1, “ruby for dummies”)
-# article2 = Article.new(author1, magazine1, “ruby for beginners”)
