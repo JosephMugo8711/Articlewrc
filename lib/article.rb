e2f839341172f07b3608222da7e6d32758a5926b
Joseph <wamiti8711@gmail.com>
2023-02-20 15:23:21 -0500

    article initialization

--- lib/article.rb
+++ lib/article.rb
@@ -1,17 +1,17 @@
 class Article
     attr_reader :author, :magazine, :title
-    @@all = []
+    # @@all = []
     
     def initialize(author, magazine, title)
       @author = author
       @magazine = magazine
       @title = title
-      @@all << self
+      # @@all << self
     end
     
-    def self.all
-      @@all
-    end
+    # def self.all
+    #   @@all
+    # end
   end
   
   
